86193aed423e5f76e2ee23e27d4f751e0303bc09
Juan Cano <jcano486@gmail.com>
2026-05-15 09:54:41 -0400

    Update frame sampling for Render video stream

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ MIN_CONFIDENCE = 0.50
 # 1 = every frame, best accuracy
 # 2 = every other frame
 # 4 = every 4th frame
-PROCESS_EVERY_N_FRAMES = 30
+PROCESS_EVERY_N_FRAMES = 90
 
 # How close a new person detection must be to an existing person
 # to be considered the same person
